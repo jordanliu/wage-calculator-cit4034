3662306f9dc8f093c64d52b33f25a435096c3f96
jordanliu <jordanxavierliu@gmail.com>
2020-09-27 22:28:03 -0500

    Added /salary.php functionality and styling

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Tax Calculator
+# Wage Calculator
+
+For CIT4034 Lab Exercise - Class Exercise: Forms, POST, Double-Scripting
 
 ```
-docker build -t tax-calculator .
-docker run -d -p 8080:80 --name tax-calculator -v "$PWD":/var/www/html php:7.2-apache
+docker build -t wage-calculator .
+docker run -d -p 8080:80 --name wage-calculator -v "$PWD":/var/www/html php:7.2-apache //mac
+docker run -d -p 8080:80 --name wage-calculator -v "%cd%":/var/www/html php:7.2-apache //windows
 ```
--- salary.php
+++ salary.php
@@ -1,5 +1,4 @@
 <html>
-
 <head>
     <title>Wage Calculator</title>
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
@@ -10,30 +9,26 @@
         <h1 class="text-center text-2xl font-bold mb-6 -m-32">
             <span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500">Dear <?php echo $_POST["name"]; ?>, based on information provided:<br></span></h1>
 
-                <body>
+        <body>
+            <?php
+            $hoursPerDay = $_POST["hoursPerDay"];
+            $hourlyRate = $_POST["hourlyRate"];
+            $daysWorked = $_POST["daysWorked"];
+            $dailyTravelExpense = $_POST["dailyTravelExpense"];
+            $dailyLunchExpense = $_POST["dailyLunchExpense"];
+            $weeklyWages = ($hourlyRate * $hoursPerDay) * $daysWorked - ($dailyTravelExpense * $daysWorked) - ($dailyLunchExpense * $daysWorked);
+            $monthlyWages = $weeklyWages * 4.28;
 
-<?php
-
-$hoursPerDay = $_POST["hoursPerDay"];
-$hourlyRate = $_POST["hourlyRate"];
-$daysWorked = $_POST["daysWorked"];
-$dailyTravelExpense = $_POST["dailyTravelExpense"];
-$dailyLunchExpense = $_POST["dailyLunchExpense"];
-$weeklyWages = ($hourlyRate * $hoursPerDay) * $daysWorked - ($dailyTravelExpense * $daysWorked) - ($dailyLunchExpense * $daysWorked);
-$monthlyWages = $weeklyWages * 4.28;
-
-echo "<p> Hourly Rate: $" . $hourlyRate . "</p>";
-echo "<p> Hours: " . $hoursPerDay . "</p>";
-echo "<p> Days Worked: " . $daysWorked . "</p>";
-echo "<p> Daily Travel Expense: $" . $dailyTravelExpense . "</p>";
-echo "<p> Daily Lunch Expense: $" . $dailyLunchExpense . "</p>";
-echo "<p> Weekly Wages: $" . $weeklyWages . "</p>";
-echo "<p> Monthly Wages: $" . $monthlyWages . "</p>";
-
-?>
-
-<a class="mt-6 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500" href="/">Click here to calculate again</a>
-</body>
+            echo "<p><span class='font-bold'>Hourly Rate:</span> $" . $hourlyRate . "</p>";
+            echo "<p><span class='font-bold'>Hours:</span> " . $hoursPerDay . "</p>";
+            echo "<p><span class='font-bold'>Days Worked:</span> " . $daysWorked . "</p>";
+            echo "<p><span class='font-bold'>Daily Travel Expense:</span> $" . $dailyTravelExpense . "</p>";
+            echo "<p><span class='font-bold'>Daily Lunch Expense:</span> $" . $dailyLunchExpense . "</p>";
+            echo "<p><span class='font-bold'>Weekly Wages:</span> $" . $weeklyWages . "</p>";
+            echo "<p><span class='font-bold'>Monthly Wages:</span> $" . $monthlyWages . "</p>";
+            ?>
+            <a class="mt-6 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500" href="/">Click here to calculate again</a>
+        </body>
     </div>
 </body>
 </html>
